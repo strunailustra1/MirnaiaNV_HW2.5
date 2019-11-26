86302da23f980d602b93190af87c7454ece59b11
Natalia Mirnaia <strunailustra@gmail.com>
2019-11-26 22:40:26 +0300

    add gradient

--- MirnaiaNV_HW2.5/WelcomeViewController.swift
+++ MirnaiaNV_HW2.5/WelcomeViewController.swift
@@ -17,5 +17,28 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         login.text = "Welcome, \(loginText!)!"
+        
+        initGradient()
+    }
+    
+    private func initGradient() {
+        let colors = Colors()
+        view.backgroundColor = UIColor.clear
+        let backgroundLayer = colors.gl
+        backgroundLayer!.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer!, at: 0)
+    }
+}
+
+class Colors {
+    var gl:CAGradientLayer!
+
+    init() {
+        let colorTop = UIColor(red: 115.0 / 255.0, green: 252.0 / 255.0, blue: 214.0 / 255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 0.0 / 255.0, green: 144.0 / 255.0, blue: 81.0 / 255.0, alpha: 1.0).cgColor
+
+        self.gl = CAGradientLayer()
+        self.gl.colors = [colorTop, colorBottom]
+        self.gl.locations = [0.0, 1.0]
     }
 }
